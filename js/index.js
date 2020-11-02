99cc2b218e40f273cda03d3104e82e636c19477f
Buvaneshvaran <52979995+MrX1068@users.noreply.github.com>
2020-11-02 18:10:58 +0530

    everthing finished,...  :)

--- js/index.js
+++ js/index.js
@@ -1,73 +1,8 @@
 
-// //  var customClickColor = document.getElementById("linux");
 
-// //  customClickColor.onclick = function() {
-// //      customClickColor.style.background = "#5fa6d7"
-// //  }
-
-
-// var down = document.getElementById("download");
-
-// down.onclick = function () {
-//     alert("innum add panala aprm try pannavum...yours faithfully  :) ");
-// }
-
-// /* mail link */
-
-// // var mail = document.getElementById("mail");
-
-// // mail.onclick = function () {
-// //     window.location.href = "mailto:buvaneshvaran1068@gmail.com" ;
-// // }
-
-// /* githublink */
-// var github = document.getElementById("github");
-
-// github.onclick = function () {
-//     window.location.href = "https://github.com/MrX1068";
-// }
-
-// /* linkedin link */
-
-// var linkedin = document.getElementById("linkedin");
-
-// linkedin.onclick = function () {
-//     window.location.href = "https://in.linkedin.com/in/mrx1068";
-// }
-
-// /* telegram link */
-
-// var telegram = document.getElementById("telegram");
-
-// telegram.onclick = function () {
-//     window.location.href = "https://t.me/mrx1068";
-// }
-
-// /* wapplink */
-// // var wapp = document.getElementById("wapp");
-
-// // wapp.onclick = function () {
-// //     window.location.href = "https://wa.me/917358891043" ;
-// // }
-
-// /* instalink */
-// // var insta = document.getElementById("insta");
-
-// // insta.onclick = function () {
-// //     window.location.href = "" ;
-// // }
-
-// $(function () {
-//     $('[data-toggle="tooltip"]').tooltip();
+// $(window).on('load', function() {
+//   $('#mdb-preloader').delay(1000).fadeOut(300);
 // });
-
-// $(document).ready(function () {
-//     new WOW().init();
-// });
-
-$(window).on('load', function() {
-  $('#mdb-preloader').delay(1000).fadeOut(300);
-});
 
 $( document ).ready(function() {
   new WOW().init();
